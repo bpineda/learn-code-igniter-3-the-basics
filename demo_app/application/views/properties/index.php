d4ab2735e53d52b96143ec7e71640a2061f2e867
bpineda <_@bernardopineda.mx>
2016-08-21 16:34:18 -0500

    Added more elements to the details page

--- demo_app/application/views/properties/index.php
+++ demo_app/application/views/properties/index.php
@@ -1,12 +1,20 @@
 <div>
   <div class="row column text-center">
-    <h2>Real State MX</h2>
+    <h2>Real State Application</h2>
   </div>
 </div>
 <hr>
 
 <div class="row column">
-  <h3>Properties details</h3>
+  <div class="row medium-up-2 large-up-2">
+    <div class="column text-left">
+    <h3>Properties</h2>
+    </div>
+    <div class="column text-right">
+      <a class="button secondary hollow" href="#" ><i class="fa fa-globe" aria-hidden="true"></i> Export</a>
+      <a class="button secondary hollow" href="#" ><i class="fa fa-table" aria-hidden="true"></i> Export</a>
+    </div>
+    </div>
   <table border="0">
     <tr>
       <td>IMAGE</td>
@@ -17,11 +25,11 @@
     </tr>
     <tr>
       <td><img src="https://s-media-cache-ak0.pinimg.com/736x/66/3c/25/663c2510e0d56d893a48030a2108f0b0.jpg" width="150" /></td>  
-      <td>Heffner Mansion</td>
-      <td>Hamptons, New York</td>
-      <td>Available</td>
+      <td>Evans Mansion</td>
+      <td>Los Angeles, California</td>
+      <td><span class="success label">Available</span></td>
       <td>
-        <a class="button alert">Remove Highlight</a>
+        <a class="button" href="<?php echo site_url('properties/show/')  ?>" >View Details</a>
       </td>  
     </tr>
 
@@ -29,19 +37,29 @@
       <td><img src="https://elevateliving.com/images/mansions-at-jordan-creek/community-photos/large/mjc-0421.jpg" width="150" /></td>  
       <td>Mosby Mansion </td>
       <td>Carpinteria, California</td>
-      <td>Available</td>
+      <td><span class="alert label">Sold</span></td>
       <td>
-        <a class="button success">Highlight</a>
+        <a class="button">View Details</a>
       </td>  
     </tr>
 
     <tr>
       <td><img src="http://cdn.pursuitist.com/wp-content/uploads/2015/05/Mean_girls_Mansion_Toronto_main-e1432549364859.jpeg" width="150" /></td>  
-      <td>The NBA mansion</td>
-      <td>Chicago, Illinois</td>
-      <td>Available</td>
+      <td>The Pineda Mansion</td>
+      <td>Hollywood, California</td>
+      <td><span class="success label">Available</span></td>
       <td>
-        <a class="button success">Highlight</a>
+        <a class="button">View Details</a>
+      </td>  
+    </tr>
+
+    <tr>
+      <td><img src="http://cdn.pursuitist.com/wp-content/uploads/2015/05/Mean_girls_Mansion_Toronto_main-e1432549364859.jpeg" width="150" /></td>  
+      <td>The Kirk Mansion</td>
+      <td>Hollywood, California</td>
+      <td><span class="warning label">Committed</span></td>
+      <td>
+        <a class="button">View Details</a>
       </td>  
     </tr>
   </table>
