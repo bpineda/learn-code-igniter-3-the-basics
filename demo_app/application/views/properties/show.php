be1881a175b2f863151ffcc2836179a854f570eb
bpineda <_@bernardopineda.mx>
2016-08-21 16:31:51 -0500

    Added a show view

---
+++ demo_app/application/views/properties/show.php
@@ -0,0 +1,49 @@
+<div class="row columns">
+&nbsp;
+</div>
+<div class="row">
+  <div class="medium-6 columns">
+    <span class="success label">Available</span>
+    <img class="thumbnail" src="http://placehold.it/650x350">
+    <div class="row small-up-4">
+    </div>
+  </div>
+  <div class="medium-6 large-5 columns">
+    <h3>Evans Mansion</h3>
+    <p>
+    3410 W Chadick Street<br/>
+    9999, Los Angeles, California
+    </p>
+
+    <div class="callout primary">
+      <h5>Description</h5>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, eum perferendis nulla quisquam similique voluptas reprehenderit quae dolores eius vel nobis autem.
+      </p>
+    </div>
+    <h5>Real Estate Agent</h5>
+    <a class="button hollow success" href="">Agency 123</a>
+  </div>
+</div>
+<div class="column row">
+  <hr>
+  <ul class="tabs" data-tabs id="example-tabs">
+    <li class="tabs-title is-active"><a href="#panel1" aria-selected="true">Location</a></li>
+  </ul>
+  <div class="tabs-content" data-tabs-content="example-tabs">
+    <div class="tabs-panel is-active" id="panel1">
+
+    <div class="media-object stack-for-small">
+      <div class="media-object-section">
+      <img class="thumbnail" src="http://placehold.it/800x400">
+      </div>
+      <div class="media-object-section">
+        <span class="secondary label">Latitude</span>
+        <input type="text" id="middle-label" placeholder="19.432608" value="34.052234">
+        <span class="secondary label">Longitude</span>
+        <input type="text" id="middle-label" placeholder="-99.133208" value="-118.243685">
+      </div>
+    </div>
+    </div>
+  </div>
+</div>
