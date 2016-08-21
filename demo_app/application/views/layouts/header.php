990223872bab0ac1b81fc3b6dcb79f6f983dbfec
bpineda <_@bernardopineda.mx>
2016-08-21 16:33:19 -0500

    Loaded font awesome and changed the application title

--- demo_app/application/views/layouts/header.php
+++ demo_app/application/views/layouts/header.php
@@ -20,8 +20,10 @@
     <link href="<?php echo base_url('foundation/css/foundation.css'); ?>" rel="stylesheet" type="text/css" />
     <script type="text/javascript" src="<?php echo base_url('foundation/js/foundation.js'); ?>"></script>
 
+    <!-- Font awesome -->
+    <link href="<?php echo base_url('font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet" type="text/css" />
 
-    <title>CodeIgniter Base App</title>
+    <title>Real Estate App</title>
     </head>
     <body class="">
         <input type="hidden" id="hidSiteURL" value="<?php echo site_url()?>">
