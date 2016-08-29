17a8d3908fbda846b5e2e407666c2bb585ca8aa5
bpineda <_@bernardopineda.mx>
2016-08-29 14:36:59 -0700

    Added correct assets to properties

--- ExerciseFiles/Ch01/01_02_start/application/views/properties/index.php
+++ ExerciseFiles/Ch01/01_02_start/application/views/properties/index.php
@@ -19,9 +19,9 @@
       <td>ACTION</td>  
     </tr>
     <tr>
-      <td><img src="https://s-media-cache-ak0.pinimg.com/736x/66/3c/25/663c2510e0d56d893a48030a2108f0b0.jpg" width="150" /></td>  
-      <td>Heffner Mansion</td>
-      <td>Hamptons, New York</td>
+      <td><img src="http://localhost:8080/assets/images/ThinkstockPhotos-145054512.jpg" width="150" /></td>  
+      <td>4 Bedroom 2 Story House </td>
+      <td>Greeley, CO </td>
       <td>Available</td>
       <td>
         <a class="button success">View Details</a>
@@ -29,9 +29,9 @@
     </tr>
 
     <tr>
-      <td><img src="https://elevateliving.com/images/mansions-at-jordan-creek/community-photos/large/mjc-0421.jpg" width="150" /></td>  
-      <td>Mosby Mansion </td>
-      <td>Carpinteria, California</td>
+      <td><img src="http://localhost:8080/assets/images/ThinkstockPhotos-160415922.jpg" width="150" /></td>  
+      <td>Spacious Classic Home</td>
+      <td>Loveland, CO </td>
       <td>Available</td>
       <td>
         <a class="button success">View Details</a>
@@ -39,9 +39,9 @@
     </tr>
 
     <tr>
-      <td><img src="http://cdn.pursuitist.com/wp-content/uploads/2015/05/Mean_girls_Mansion_Toronto_main-e1432549364859.jpeg" width="150" /></td>  
-      <td>The NBA mansion</td>
-      <td>Chicago, Illinois</td>
+      <td><img src="http://localhost:8080/assets/images/ThinkstockPhotos-179240420.jpg" width="150" /></td>  
+      <td>3 Bedroom Condo</td>
+      <td>Platteville, CO</td>
       <td>Available</td>
       <td>
         <a class="button success">View Details</a>
