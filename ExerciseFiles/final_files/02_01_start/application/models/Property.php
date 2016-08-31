94d86ace84cfda2f2ee713932e2581b00692774c
bpineda <_@bernardopineda.mx>
2016-08-31 10:37:04 -0700

    Added 02_01 start files

---
+++ ExerciseFiles/final_files/02_01_start/application/models/Property.php
@@ -0,0 +1,21 @@
+<?php 
+class Property extends CI_Model
+{
+
+   public function __construct()
+   {
+       parent::__construct();
+   }
+
+   public function get()
+   {
+   	return "4 Bedroom 2 Story House";
+   }
+
+   public function connection_test()
+   {
+   	$this->load->database('default', TRUE);
+   }
+
+   
+}
