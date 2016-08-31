1fadee4078b23246e10327a7b9586b06ef6f42ec
bpineda <_@bernardopineda.mx>
2016-08-31 16:37:55 -0700

    Added 02_03 final files

---
+++ ExerciseFiles/final_files/02_03_final/application/views/properties/edit.php
@@ -0,0 +1,20 @@
+<div>
+  <div class="row column text-center">
+  </div>
+</div>
+<hr>
+
+<div class="row column">
+  <h3>Edit details</h3>
+  <div class="row column text-left">
+    <form method="post">
+      <label>Name</label>
+      <input type="text" name="name" value="<?php echo $property['name'] ?>" />
+      <label>Description</label>
+      <textarea name="description"><?php echo $property['description'] ?></textarea>
+      <input class="button success" type="submit" value="SAVE" /> 
+    </form>
+    </div>
+  <br/>
+</div>
+
