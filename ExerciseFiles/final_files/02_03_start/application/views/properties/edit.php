0edb2338044cdb492c5260325c5fe87067a8c887
bpineda <_@bernardopineda.mx>
2016-08-31 15:43:57 -0700

    Added exercise 02_03 initial files

---
+++ ExerciseFiles/final_files/02_03_start/application/views/properties/edit.php
@@ -0,0 +1,20 @@
+<div>
+  <div class="row column text-center">
+  </div>
+</div>
+<hr>
+
+<div class="row column">
+  <h3>Edit details</h3>
+  <div class="row column text-left">
+    <form>
+      <label>Name</label>
+      <input type="text" />
+      <label>Description</label>
+      <textarea></textarea>
+      <input class="button success" type="submit" value="SAVE" /> 
+    </form>
+    </div>
+  <br/>
+</div>
+
---
+++ ExerciseFiles/final_files/02_03_start/application/views/properties/index.php
@@ -0,0 +1,38 @@
+<div>
+  <div class="row column text-center">
+    <h2>Welcome, <?php echo $user_name; ?></h2>
+    <select>
+        <?php foreach($status_group as $status){ ?>
+          <option><?php echo $status; ?></option>
+          <?php } ?>
+    </select>
+  </div>
+</div>
+<hr>
+
+<div class="row column">
+  <h3>Properties details</h3>
+  <table border="0">
+    <tr>
+      <td>IMAGE</td>
+      <td>NAME</td>
+      <td>LOCATION</td>
+      <td>STATUS</td>  
+      <td>ACTION</td>  
+    </tr>
+    <?php foreach($properties as $property){ ?>
+    <tr>
+      <td><img src="<?php echo base_url("assets/images/{$property['image']}"); ?>" width="150" /></td>  
+      <td><?php echo $property['name'] ?></td>
+      <td><?php echo $property['city'] ?>, <?php echo $property['state'] ?> </td>
+      <td>Available</td>
+      <td>
+        <a href="<?php echo site_url('properties/show/' . $property['id']); ?>" class="button success">View Details</a>
+        <a href="<?php echo site_url('properties/edit/' . $property['id']); ?>" class="button">Edit Details</a>
+      </td>  
+    </tr>
+    <?php } ?>
+  </table>
+  <br/>
+</div>
+
