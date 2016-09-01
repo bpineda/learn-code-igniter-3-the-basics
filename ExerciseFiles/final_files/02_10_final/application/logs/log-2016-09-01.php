5e49234b2c786ccc73a35c777dca2a61661165e2
bpineda <_@bernardopineda.mx>
2016-09-01 12:43:55 -0700

    Added 03_10 start files

---
+++ ExerciseFiles/final_files/02_10_final/application/logs/log-2016-09-01.php
@@ -0,0 +1,24 @@
+DEBUG - 2016-09-01 14:06:53 --> UTF-8 Support Enabled
+DEBUG - 2016-09-01 14:06:53 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-09-01 14:06:53 --> My First Log
+DEBUG - 2016-09-01 14:06:53 --> Total execution time: 0.0151
+DEBUG - 2016-09-01 14:08:14 --> UTF-8 Support Enabled
+DEBUG - 2016-09-01 14:08:14 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-09-01 14:08:14 --> Form parametersArray
+(
+)
+
+DEBUG - 2016-09-01 14:08:14 --> Total execution time: 0.0185
+DEBUG - 2016-09-01 14:08:16 --> UTF-8 Support Enabled
+DEBUG - 2016-09-01 14:08:16 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-09-01 14:08:16 --> Form parametersArray
+(
+    [name] => New name
+    [description] => New description
+)
+
+DEBUG - 2016-09-01 14:08:16 --> You did not select a file to upload.
+DEBUG - 2016-09-01 14:08:16 --> UTF-8 Support Enabled
+DEBUG - 2016-09-01 14:08:16 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-09-01 14:08:16 --> My First Log
+DEBUG - 2016-09-01 14:08:16 --> Total execution time: 0.0144
